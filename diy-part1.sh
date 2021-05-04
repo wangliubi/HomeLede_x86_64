1bb15160b620ed2838f607b6726c349420e2934a
wangliubi <61959515+wangliubi@users.noreply.github.com>
2021-05-04 19:51:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,30 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#git lua-maxminddb 依赖
+git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+#helloworld
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+#ssrplus
+git clone https://github.com/fw876/helloworld package/helloworld
+#passswall
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+#微信推送
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#京东
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+#smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+#smartdns界面
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+
+# argon主题
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+# Edge主题
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
